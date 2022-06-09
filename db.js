2d0b6939e7b6760e74efcc84a104e15a87a56bfb
unknown <dave8git@gmail.com>
2022-06-09 20:26:58 +0200

    initial commit

---
+++ db.js
@@ -0,0 +1,29 @@
+const db = {
+    testimonials: [
+        { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+        { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+        { id: 3, author: 'Dave Pol', text: 'Awesome! I\'ll be back' },
+        { id: 4, author: 'Mike Doe', text: 'What a great place!' },
+        { id: 5, author: 'Logan Paul', text: 'Cool stuff here!' },
+        { id: 6, author: 'Larry White', text: 'Clean and tidy.' },
+        { id: 7, author: 'Michael Jackson', text: 'Bille Jean would stay here!' },
+        { id: 8, author: 'Donald Trump', text: 'A bit too poor for me, but ok.' },
+        { id: 9, author: 'Harry Potter', text: 'Pure Magic!' },
+        { id: 10, author: 'Johnny Depp', text: 'Would not come here with Amber! Awesome place!' },  
+    ],
+    concerts: [
+        { id: 1, performer: 'John Doe', genre: 'Rock', price: 25, day: 1, image: '/img/uploads/1fsd324fsdg.jpg' },
+        { id: 2, performer: 'Rebekah Parker', genre: 'R&B', price: 25, day: 1, image: '/img/uploads/2f342s4fsdg.jpg' },
+        { id: 3, performer: 'Maybell Haley', genre: 'Pop', price: 40, day: 1, image: '/img/uploads/hdfh42sd213.jpg' },
+    ],
+    seats: [
+        { id: 1, day: 1, seat: 3, client: 'Amanda Doe', email: 'amandadoe@example.com' },
+        { id: 2, day: 1, seat: 9, client: 'Curtis Johnson', email: 'curtisj@example.com'  },
+        { id: 3, day: 1, seat: 10, client: 'Felix McManara', email: 'felxim98@example.com'  },
+        { id: 4, day: 1, seat: 26, client: 'Fauna Keithrins', email: 'mefauna312@example.com'  },
+        { id: 5, day: 2, seat: 1, client: 'Felix McManara', email: 'felxim98@example.com'  },
+        { id: 6, day: 2, seat: 2, client: 'Molier Lo Celso', email: 'moiler.lo.celso@example.com'  },
+    ],
+} 
+
+module.exports = db; 
---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const randGen = require('@dave8git/randomid-generator');
+let db = require('./db');
+const app = express();
+
+//console.log(db.testimonials);
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
+
+app.get('/testimonials', (req, res) => {
+    res.json(db.testimonials);
+});
+
+app.get('/testimonials/random', (req, res) => {
+    const index = Math.floor(Math.random() * (db.length));
+    console.log(index);
+    res.json(db.testimonials[index]);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+    res.json(db.testimonials.find(el => el.id == req.params.id));
+});
+
+app.post('/testimonials', (req, res) => {
+    const answer = { id: randGen(), author: req.body.author, text: req.body.text };
+    console.log(answer);
+    testimonials.push(answer);
+    res.json({ message: 'OK' });
+});
+
+app.put('/testimonials/:id', (req, res) => {
+
+    const existingElement = db.testimonials.find(el => el.id == req.params.id);
+    db.testimonials = db.testimonials.map(el => {
+        if(el.id == req.params.id) {
+            el.author = req.body.author; 
+            el.text = req.body.text;
+        }
+        return el;
+    });
+    res.send({message: 'OK'});
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+    db.testimonials = db.testimonials.filter(el => el.id != req.params.id);
+    res.send({ message: 'OK' });
+});
+console.log(db.concerts);
+app.get('/concerts', (req, res) => {
+    res.json(db.concerts);
+}); 
+// app.get('/concerts/:id', (req, res) => {
+//     res.json(db.concerts.find(el => el.id == req.params.id));
+// });
+app.use((req, res) => {
+    res.status(404).send('404 not found...');
+});
+app.listen(6000, () => {
+    console.log('Server is runing on port: 6000');
+});
+
+
+
+
