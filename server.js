ef11beeaaed23716bbec37c665825e0317a51298
unknown <dave8git@gmail.com>
2022-06-28 20:41:47 +0200

    fix new yarn.lock created

--- server.js
+++ server.js
@@ -14,16 +14,25 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 app.use('/api', testimonials);
 app.use('/api', concerts);
 app.use('/api', seats);
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 });
-app.listen(5000, () => {
-    console.log('Server is runing on port: 5000');
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Server is running on port: 5000');
 });
+
 
 
 
